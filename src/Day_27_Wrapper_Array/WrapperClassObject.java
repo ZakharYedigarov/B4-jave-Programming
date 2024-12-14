17a0aea7e24586a88a6b395fb7ac42757ecfbd22
ZakharYedigarov <yedigarov17@gmail.com>
2024-12-14 12:03:59 -0800

    ArrayList

---
+++ src/Day_27_Wrapper_Array/WrapperClassObject.java
@@ -0,0 +1,10 @@
+package Day_27_Wrapper_Array;
+
+public class WrapperClassObject {
+    public static void main(String[] args) {
+        Integer a = 5;
+        // Integer a = new Integer(10); this style has been deprecated / no longer is being used
+
+        int a2 = 10; //  this is a primitive data type for variable 'a'
+    }
+}
