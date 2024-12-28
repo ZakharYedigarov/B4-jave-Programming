246c5d9b0ba17ea4504785e98a8711c98c3c3fce
ZakharYedigarov <Yedigarov17@gmail.com>
2024-12-27 19:04:46 -0800

    custome class and constracot chaning

---
+++ src/Day_32_Custom_Classes/ChainExample.java
@@ -0,0 +1,29 @@
+package Day_32_Custom_Classes;
+
+public class ChainExample {
+    //first constructor non parameterized
+    public ChainExample (){
+        System.out.println("First");
+    }
+    // 2nd Constructor - parameterized (int)
+    public ChainExample(int i){
+        this();
+        System.out.println("Second");
+
+    }
+    // 3rd Constructor - parameterized (String)
+    public ChainExample(String str){
+        this(4);
+        System.out.println("Third");
+
+    }
+}
+
+/* Constructors
+  Rules for Constructor Chaining
+   1 - this() should always be first
+   2 - only one constructor can be called in the same constructor
+   3 - constructor should not call itself
+   4 - Constructors should not contain itself in the chaining with multiple constructors
+
+ */
